1771eae846fcdea66cc1eaf660626a95a1c61753
tominehaug <tomine_haug@live.no>
2026-09-15 13:36:14 +0200

    utils to format date

---
+++ js/utils/formatDate.js
@@ -0,0 +1,9 @@
+export function formatDate(dateString) {
+  return new Date(dateString).toLocaleString("en-GB", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+}
