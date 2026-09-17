9ce5aec5890d1ecb4178423793066f5fba248eb8
tominehaug <tomine_haug@live.no>
2026-09-17 14:18:16 +0200

    improved styling on card component and fixed broken path

--- js/components/card.js
+++ js/components/card.js
@@ -1,4 +1,4 @@
-import { formatDate } from "../utils/formatDate";
+import { formatDate } from "../utils/formatDate.js";
 
 const basePath = window.location.hostname.includes("github.io")
   ? "/blitzbid"
@@ -7,7 +7,11 @@ const basePath = window.location.hostname.includes("github.io")
 const profile = JSON.parse(localStorage.getItem("profile"));
 
 export function renderCard(auction, container) {
-  const highestBid = auction.bids.length
+  if (!auction.bids) {
+    console.warn("Listing missing 'bids':", auction);
+  }
+
+  const highestBid = auction.bids?.length
     ? Math.max(...auction.bids.map((bid) => bid.amount))
     : 0;
 
@@ -16,17 +20,27 @@ export function renderCard(auction, container) {
   const cardWrapper = document.createElement("a");
   cardWrapper.classList.add(
     "font-heading",
-    "bg-white",
     "relative",
-    "w-20",
+    "mx-4",
+    "my-2",
+    "flex",
+    "w-90",
+    "flex-col",
+    "items-center",
+    "justify-center",
     "overflow-hidden",
+    "bg-white",
   );
   cardWrapper.href = `${basePath}/auction.html?id=${auction.id}`;
 
   const thumbnail = document.createElement("img");
-  thumbnail.src = auction.media[0].url;
-  thumbnail.alt = auction.media[0].alt;
-  thumbnail.classList.add("h-50", "w-full", "object-cover");
+  thumbnail.src = auction.media[0]?.url || "../../assets/placeholder-img.png";
+  thumbnail.alt = auction.media[0]?.alt || "No image uploaded";
+  thumbnail.classList.add("h-70", "w-full", "object-cover");
+  thumbnail.addEventListener("error", () => {
+    thumbnail.src = "../../assets/placeholder-img.png";
+    thumbnail.alt = "Image unavailable";
+  });
 
   const details = document.createElement("div");
   details.classList.add("p-5");
@@ -45,11 +59,14 @@ export function renderCard(auction, container) {
   price.append(amount, " (highest bid)");
 
   const deadline = document.createElement("p");
-  deadline.classList.add("text-error");
+  deadline.classList.add("mt-2", "border-t", "border-gray-400", "text-xl");
   deadline.textContent = `Deadline: ${endsAt}`;
 
-  details.appendChild(title, price, deadline);
+  details.appendChild(title);
+  details.appendChild(price);
+  details.appendChild(deadline);
   cardWrapper.appendChild(thumbnail);
+  cardWrapper.appendChild(details);
 
   if (auction.seller.name === profile?.name) {
     const editIcon = document.createElement("i");
@@ -57,8 +74,9 @@ export function renderCard(auction, container) {
       "fa-solid",
       "fa-pen-to-square",
       "absolute",
-      "top-1",
-      "right-1",
+      "top-2",
+      "right-2",
+      "text-3xl",
     );
     cardWrapper.appendChild(editIcon);
   }
