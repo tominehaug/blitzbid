ea1941a2184e8e1ff9004d3b05eab146b59b6a84
tominehaug <tomine_haug@live.no>
2026-09-19 17:54:06 +0200

    updated icon size

--- js/components/header.js
+++ js/components/header.js
@@ -27,14 +27,14 @@ export function renderHeader() {
       <a href="${basePath}/index.html">
         <img src="${basePath}/assets/logo.svg" alt="BlitzBid logo" width="180" height="72"/>
       </a>
-      <a href="${basePath}/login.html"><i class="fa-solid fa-right-to-bracket text-5xl"></i></a>
+      <a href="${basePath}/login.html"><i class="fa-solid fa-right-to-bracket text-3xl"></i></a>
     `;
   } else if (path.includes("profile.html") && user === profile?.name) {
     header.innerHTML = `
       <a href="${basePath}/index.html">
         <img src="${basePath}/assets/logo.svg" alt="BlitzBid logo" width="180" height="72"/>
       </a>
-      <button id="logout"><i class="fa-solid fa-arrow-right-from-bracket text-5xl"></i></button>
+      <button id="logout"><i class="fa-solid fa-arrow-right-from-bracket text-3xl"></i></button>
     `;
 
     const logoutBtn = document.getElementById("logout");
@@ -64,7 +64,7 @@ export function renderHeader() {
       <a href="${basePath}/index.html">
         <img src="${basePath}/assets/logo.svg" alt="BlitzBid logo" width="180" height="72"/>
       </a>
-      <a href="${basePath}/profile.html?user=${username}"><i class="fa-solid fa-circle-user text-5xl"></i></a>
+      <a href="${basePath}/profile.html?user=${username}"><i class="fa-solid fa-circle-user text-3xl"></i></a>
     `;
   }
 }
