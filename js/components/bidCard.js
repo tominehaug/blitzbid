b2162b3bed257d6e46269f7f397de52633c55863
tominehaug <tomine_haug@live.no>
2026-09-23 15:10:59 +0200

    styled avatar img

--- js/components/bidCard.js
+++ js/components/bidCard.js
@@ -13,7 +13,7 @@ export function renderBidCard(bid, container) {
     avatar.src = bid.bidder.avatar.url;
   }
   avatar.alt = bid.bidder?.avatar?.alt;
-  avatar.className = "h-full aspect-square object-cover";
+  avatar.className = "h-full aspect-square object-cover rounded-full";
 
   const details = document.createElement("div");
   details.className = "flex flex-col items-end justify-end p-1";
